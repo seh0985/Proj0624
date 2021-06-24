db4d4eb132ac2f46e36f9f321b165334d9fb1e76
jaeyoung <seh0985@naver.com>
2021-06-24 15:48:16 +0900

    abcd

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace FirstProj
+{
+    class Program
+    {
+        
+        static int numA = 99;
+
+        string chA = "abcde";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello jaey!");
+           // Console.WriteLine(numA);
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(numA + i ) ;
+           
+            }
+            */
+            Program proA = new Program();
+            proA.doThat();
+        }
+
+        void doThat()
+        {
+        Console.WriteLine(chA) ;          
+        }
+    }
+}
